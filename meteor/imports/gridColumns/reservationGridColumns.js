0c6a42245c4bc1224a10c6188f85376431d7aff1
joe_demilo <joe.demilo@bankersfinancialcorp.com>
2016-09-02 13:11:23 -0400

    Action initiator column rendering. Still needs wired up and responsive look finished though

--- meteor/imports/gridColumns/reservationGridColumns.js
+++ meteor/imports/gridColumns/reservationGridColumns.js
@@ -1,8 +1,17 @@
-// dateTime", "name", "partyNumber", "status"'
+import {reactCellRendererFactory} from 'ag-grid-react';
+import {reactFilterFactory} from 'ag-grid-react';
+
+import GridActionColumn from '../components/gridActionColumn'
+import reservationActionTypes from '../actionTypes/reservationActionTypes'
 
 export default columnDefs = [
   {field: "dateTime", headerName: "Date & Time"},
   {field: "name", headerName: "Name"},
   {field: "partyNumber", headerName: "Number"},
-  {field: "status", headerName: "Status"}
+  {field: "status",
+    headerName: "Status",
+    collapseOnScreenWidth: 1000,
+    cellRenderer: reactCellRendererFactory(GridActionColumn),
+    changeInitiators: reservationActionTypes.changeIniators
+  }
 ];
